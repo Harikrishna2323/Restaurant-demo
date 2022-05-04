781a662d80e72d6249b723f8a16ba6071c3fadd7
harikrishna2323 <harikb23@gmail.com>
2022-05-05 00:37:05 +0530

    changes in server config and plugRoots

--- restaurant-server/src/schemas/plugRoots.ts
+++ restaurant-server/src/schemas/plugRoots.ts
@@ -1,13 +1,21 @@
 import { dishSchema } from './dishSchema';
 import { orderSchema } from './orderSchema';
 import { userSchema } from "./userSchema";
+import { gql } from "apollo-server-express";
 import { makeExecutableSchema, mergeSchemas } from "graphql-tools";
 
+export const customTypeDefs = gql`
+  scalar DateTime
 
+  type ResponseMetaData {
+    success: Boolean!
+    message: String!
+  }
+`;
 
 
 const executableSchema = makeExecutableSchema({
-  typeDefs: [userSchema, orderSchema, dishSchema],
+  typeDefs: [customTypeDefs, userSchema, orderSchema, dishSchema],
   resolvers: [],
 });
 
--- restaurant-server/src/server.ts
+++ restaurant-server/src/server.ts
@@ -1,4 +1,5 @@
-
+import { Image } from './entities/Image';
+import { UserRoles } from './entities/UserRoles';
 import express from "express";
 require("dotenv").config();
 
@@ -15,19 +16,27 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 
-function initDB(){
-    return createConnection({
+async function initDB(){
+
+    return await createConnection({
         type: "mysql",
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: 3306,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_SCHEMA,
-        entities: [Users, Dishes, Orders],
+        entities: [Users, Dishes, Orders, UserRoles, Image],
         synchronize: false,
+        logging: !!process.env.LOG_TYPEORM,
     })
 }
+// DB_HOST=localhost
+// DB_PORT=3306
+// DB_USER=root
+// DB_PASSWORD=hari1234
+// DB_SCHEMA=restaurant
+// LOG_TYPEORM=true
 
-initDB().then(() =>{
-    app.listen(PORT, ()=> console.log(`App started in port ${PORT}`) )
-})
+initDB().then(()=> app.listen(PORT, () => console.log(`Server started on port: ${PORT}`)));
+
+
